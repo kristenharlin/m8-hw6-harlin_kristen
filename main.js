11df8ce2314e22e79f1a605a4bd0808061b05406
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-10-13 14:59:46 -0400

    Add event handler to button

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
 
-// step 1
+// step 2
 var shakeBall = function(){
   magicFortunes.sort(function(a, b){return 0.5 - Math.random()});
-  console.log(magicFortunes);
+  console.log(magicFortunes); // to test funtionality of beginning steps
 };
+
+//step 3
+var button = document.getElementsByTagName('button')[0];
+button.onclick = shakeBall;
